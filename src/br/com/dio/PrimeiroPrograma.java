d546cb27a3f488401be0fd560994217c09c34596
Lais <laistech13@gmail.com>
2022-07-19 01:44:38 -0300

    First Commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        gato gato = new gato();
+        System.out.println(gato);
+        /*System.out.println("Hello, World!");*/
+    }
+}
